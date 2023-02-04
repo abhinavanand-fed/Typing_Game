74a61d224242ff581e413c8373f87c2034afe776
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-04 19:02:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./TypingGame.css";
 
-function App() {
+const TypingGame = () => {
+  const [wordCount, setWordCount] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    if (isTimeRunning && timeLeft > 0) {
+      setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+    } else if (timeLeft === 0) {
+      setIsTimeRunning(false);
+    }
+  }, [timeLeft, isTimeRunning]);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+    setWordCount(event.target.value.split(" ").length);
+  };
+
+  const handleStartClick = () => {
+    setIsTimeRunning(true);
+    setWordCount(0);
+    setTimeLeft(10);
+    setText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Typing Game</h1>
+      <textarea
+        className="text-area"
+        onChange={handleChange}
+        value={text}
+      />
+      <h4 className="word-count">Word Count: {wordCount}</h4>
+      <h4 className="time-left">Time Left: {timeLeft}</h4>
+      <button className="start-button" onClick={handleStartClick} disabled={isTimeRunning}>
+        Start
+      </button>
     </div>
   );
-}
+};
 
-export default App;
+export default TypingGame;
